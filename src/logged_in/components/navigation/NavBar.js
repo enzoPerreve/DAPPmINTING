b2228d2d36737889b88f71e7d5f4d680bbfd533c
Enzo PERREVE <perreve.enzo@gmail.com>
2021-10-03 23:15:36 +0200

    Ajout du Alps

--- src/logged_in/components/navigation/NavBar.js
+++ src/logged_in/components/navigation/NavBar.js
@@ -234,7 +234,7 @@ function NavBar(props) {
                 display="inline"
                 color="primary"
               >
-                Wa
+                Alps
               </Typography>
               <Typography
                 variant="h4"
@@ -242,7 +242,7 @@ function NavBar(props) {
                 display="inline"
                 color="secondary"
               >
-                Ver
+                Hunks
               </Typography>
             </Hidden>
           </Box>
